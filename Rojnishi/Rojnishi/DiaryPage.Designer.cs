ed7117175f922e3cf5355968c62bf5ac905ebee2
Sayali Lad <sayalilad7927@gmail.com>
2018-09-20 00:20:20 +0530

    Add files via upload
    basic rojnishi application

---
+++ Rojnishi/Rojnishi/DiaryPage.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Rojnishi
+{
+    partial class DiaryPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_editor = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_editor
+            // 
+            this.txt_editor.Location = new System.Drawing.Point(8, 12);
+            this.txt_editor.Multiline = true;
+            this.txt_editor.Name = "txt_editor";
+            this.txt_editor.Size = new System.Drawing.Size(470, 312);
+            this.txt_editor.TabIndex = 0;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 330);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(110, 28);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // DiaryPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 370);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txt_editor);
+            this.Name = "DiaryPage";
+            this.Text = "DiaryPage";
+            this.Load += new System.EventHandler(this.DiaryPage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_editor;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
---
+++ Rojnishi/Rojnishi/DiaryPage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rojnishi
+{
+    public partial class DiaryPage : Form
+    {
+        public DiaryPage()
+        {
+            InitializeComponent();
+        }
+        public string text_toWrite="";
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            text_toWrite = txt_editor.Text;
+            string filePath_ = DateTime.Now.Date.ToString("yyyyMMdd") + ".txt";
+           
+            using (FileStream fs = new FileStream(filePath_, FileMode.Append))
+            {
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine(text_toWrite);
+                }
+            }
+        }
+
+        private void DiaryPage_Load(object sender, EventArgs e)
+        {
+            txt_editor.AppendText("\t\t"+DateTime.Now.Date.ToLongDateString()+Environment.NewLine + Environment.NewLine);
+        }
+    }
+}
